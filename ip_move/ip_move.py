87607401767ac6d4c3f270b51cd4f47729c7ef7c
Christopher Hart (chart2) <chart2@cisco.com>
2018-05-31 20:39:44 -0400

    Initial commit

---
+++ ip_move/ip_move.py
@@ -0,0 +1,157 @@
+#! /isan/bin/python
+
+import threading
+import sys
+import nx_sdk_py
+import json
+import re
+
+cli_parser = 0
+sdk = 0
+event_hdlr = 0
+
+def evt_thread():
+    global cli_parser, sdk, event_hdlr
+
+    sdk = nx_sdk_py.NxSdk.getSdkInst(len(sys.argv), sys.argv)
+    if not sdk:
+        return
+
+    sdk.setAppDesc("IP Movement")
+
+    tracer = sdk.getTracer()
+    tracer.event("[{}] Started service".format(sdk.getAppName()))
+
+    cli_parser = sdk.getCliParser()
+
+    nx_cmd = cli_parser.newShowCmd("show_ip_movement", "<ip>")
+    nx_cmd.updateParam("<ip>", "IP address to track movement of", nx_sdk_py.P_IP_ADDR)
+
+    cmd = pyCmdHandler()
+    cli_parser.setCmdHandler(cmd)
+    cli_parser.addToParseTree()
+
+    tracer.event("[{}] Starting event loop".format(sdk.getAppName()))
+    sdk.startEventLoop()
+
+    tracer.event("Service quitting!")
+    event_hdlr = False
+
+    nx_sdk_py.NxSdk.__swig_destroy__(sdk)
+
+def get_mac_from_arp(cli_parser, clicmd, target_ip):
+    exec_cmd = "show ip arp {}".format(target_ip)
+    arp_cmd = cli_parser.execShowCmd(exec_cmd, nx_sdk_py.R_JSON)
+    if arp_cmd:
+        try:
+            arp_json = json.loads(arp_cmd)
+        except ValueError as exc:
+            return None
+        count = int(arp_json["TABLE_vrf"]["ROW_vrf"]["cnt-total"])
+        if count:
+            intf = arp_json["TABLE_vrf"]["ROW_vrf"]["TABLE_adj"]["ROW_adj"]
+            if intf.get("ip-addr-out") == target_ip:
+                target_mac = intf["mac"]
+                clicmd.printConsole("{} is currently present in ARP table, MAC address {}\n".format(target_ip, target_mac))
+                return target_mac
+            else:
+                return None
+        else:
+            return None
+    else:
+        return None
+
+def get_vlan_from_cam(cli_parser, clicmd, target_mac):
+    exec_cmd = "show mac address-table address {}".format(target_mac)
+    mac_cmd = cli_parser.execShowCmd(exec_cmd, nx_sdk_py.R_JSON)
+    if mac_cmd:
+        try:
+            cam_json = json.loads(mac_cmd)
+        except ValueError as exc:
+            return None
+        mac_entry = cam_json["TABLE_mac_address"]["ROW_mac_address"]
+        if mac_entry:
+            if mac_entry["disp_mac_addr"] == target_mac:
+                egress_intf = mac_entry["disp_port"]
+                mac_vlan = mac_entry["disp_vlan"]
+                clicmd.printConsole("{} is currently present in MAC address table on interface {}, VLAN {}\n".format(target_mac, egress_intf, mac_vlan))
+                return mac_vlan
+            else:
+                return None
+        else:
+            return None
+    else:
+        return None
+
+def find_mac_movement(cli_parser, clicmd, target_mac, mac_vlan):
+    exec_cmd = "show system internal l2fm l2dbg macdb address {} vlan {}".format(target_mac, mac_vlan)
+    l2fm_cmd = cli_parser.execShowCmd(exec_cmd)
+    if l2fm_cmd:
+        event_re = re.compile(r"^\s+(\w{3}) (\w{3}) (\d+) (\d{2}):(\d{2}):(\d{2}) (\d{4}) (0x\S{8}) (\d+)\s+(\S+) (\d+)\s+(\d+)\s+(\d+)")
+        unique_interfaces = []
+        l2fm_events = l2fm_cmd.splitlines()
+        for line in l2fm_events:
+            res = re.search(event_re, line)
+            if res:
+                day_name = res.group(1)
+                month = res.group(2)
+                day = res.group(3)
+                hour = res.group(4)
+                minute = res.group(5)
+                second = res.group(6)
+                year = res.group(7)
+                if_index = res.group(8)
+                db = res.group(9)
+                event = res.group(10)
+                src = res.group(11)
+                slot = res.group(12)
+                fe = res.group(13)
+                if "MAC_NOTIF_AM_MOVE" in event:
+                    timestamp = "{} {} {} {}:{}:{} {}".format(day_name, month, day, hour, minute, second, year)
+                    intf_dict = {"if_index": if_index, "timestamp": timestamp}
+                    unique_interfaces.append(intf_dict)
+        if not unique_interfaces:
+            clicmd.printConsole("No entries for {} in L2FM L2DBG\n".format(target_mac))
+        if len(unique_interfaces) == 1:
+            clicmd.printConsole("{} has not been moving between interfaces\n".format(target_mac))
+        if len(unique_interfaces) > 1:
+                clicmd.printConsole("{} has been moving between the following interfaces, from most recent to least recent:\n".format(target_mac))
+                unique_interfaces = get_snmp_intf_index(unique_interfaces)
+                clicmd.printConsole("\t{} - {} (Current interface)\n".format(unique_interfaces[-1]["timestamp"], unique_interfaces[-1]["intf_name"]))
+                for intf in unique_interfaces[-2::-1]:
+                    clicmd.printConsole("\t{} - {}\n".format(intf["timestamp"], intf["intf_name"]))
+
+class pyCmdHandler(nx_sdk_py.NxCmdHandler):
+    def postCliCb(self, clicmd):
+        global cli_parser
+
+        if "show_ip_movement" in clicmd.getCmdName():
+            target_ip = nx_sdk_py.void_to_string(clicmd.getParamValue("<ip>"))
+
+            target_mac = get_mac_from_arp(cli_parser, clicmd, target_ip)
+            mac_vlan = ""
+            if target_mac:
+                mac_vlan = get_vlan_from_cam(cli_parser, clicmd, target_mac)
+                if mac_vlan:
+                    find_mac_movement(cli_parser, clicmd, target_mac, mac_vlan)
+                else:
+                    print("No entires in MAC address table")
+                    clicmd.printConsole("No entries in MAC address table for {}".format(target_mac))
+            else:
+                clicmd.printConsole("No entries in ARP table for {}".format(target_ip))
+        return True
+
+def get_snmp_intf_index(if_index_dict_list):
+    global cli_parser
+
+    snmp_ifindex = cli_parser.execShowCmd("show interface snmp-ifindex", nx_sdk_py.R_JSON)
+    snmp_ifindex_json = json.loads(snmp_ifindex)
+    snmp_ifindex_list = snmp_ifindex_json["TABLE_interface"]["ROW_interface"]
+    for index_dict in if_index_dict_list:
+        index = index_dict["if_index"]
+        for ifindex_json in snmp_ifindex_list:
+            if index == ifindex_json["snmp-ifindex"]:
+                index_dict["intf_name"] = ifindex_json["interface"]
+    return if_index_dict_list
+
+evt_thread()
